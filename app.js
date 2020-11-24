b3fbe837c02239d4d7281f8280adcd285cbdd78f
linchuaccount <younchu123@gmail.com>
2020-11-25 00:40:22 +0800

    feat: install handlebars & add bootstrap (make simple layout)

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// 載入 express 
+// 載入工具套件
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 // 設定路由
 app.get('/', (req, res) => {
-  res.send('This is a restaurant_list web run by express')
+  res.render('index')
 })
 
 // 設定 port 3000
